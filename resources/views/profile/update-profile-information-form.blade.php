733e6000b136e7b2ed835be49d68ff24c0d6c627
Alberto Medina <luisitomedina95@gmail.com>
2021-03-17 10:45:05 -0600

    Fin Sprint 1
    Agrego numero de teléfono a la modificación de datos de usuario.
    Se deshabilito la edición del correo en la modificación de datos de usuario.
    Se agrego un link para registrarse en el formulario de login
    Se agrego el contrato de términos y condiciones

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -60,8 +60,16 @@
 
         <!-- Email -->
         <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="number" value="{{ __('Telefono') }}" />
+            <x-jet-input id="number" type="number" class="mt-1 block w-full" wire:model.defer="state.number" />
+            <x-jet-input-error for="number" class="mt-2" />
+        </div>
+
+    
+        <!-- Email -->
+        <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="email" value="{{ __('Email') }}" />
-            <x-jet-input id="email" type="email" class="mt-1 block w-full" wire:model.defer="state.email" />
+            <x-jet-input disabled id="email" type="email" class="mt-1 block w-full" wire:model.defer="state.email" />
             <x-jet-input-error for="email" class="mt-2" />
         </div>
     </x-slot>
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -6,7 +6,37 @@
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
-                {!! $terms !!}
+            <h1>Terminos y condiciones</h1>
+Condiciones para la publicación de un artículo o servicio 
+Cuando pones en venta un artículo o decides ofrecer un servicio, debes estar de acuerdo con las siguientes condiciones que se te presentan a continuación: 
+•	Es necesario que seas mayor de edad para poder registrarte y publicar anuncios en esta plataforma. 
+•	El contenido que publiques a través de los anuncios debe contener información verídica de los mismos, ya sea un servicio o producto que se ofrezca, de lo contrario puede ser modificado, o eliminado a nuestra discreción. 
+•	Si publicas un anuncio que contenga imágenes pornográficas, drogas o estupefacientes o contenido relacionado con el mismo, esto para ofrecer un servicio relacionado con ello, dicho anuncio será eliminado.
+•	No está permitido que dupliques un anuncio (esto no significa que no puedes publicar anuncios que tengan cosas similares). 
+•	El administrador podrá editar y eliminar cualquier anuncio considerando que no cumple con las reglas establecidas en este contrato. 
+•	Tu oferta.com no tiene ninguna responsabilidad con respecto al envió de productos comprados o vendidos en su plataforma. 
+•	Los anuncios se publicarán inicialmente por orden de fecha y hora, por lo cual tu anuncio no siempre estará entre los primeros inicialmente. 
+•	Los anuncios tendrán un límite de 90 días para estar publicados en la plataforma, al finalizar el tiempo, éstos serán eliminados automáticamente.
+•	No nos hacemos responsables si el comprador recibe un artículo que no sea de su voluntad, es decir que no sea el que vio inicialmente en la o las imágenes del anuncio que le interesó. 
+•	No es responsabilidad nuestra si en un anuncio se postean imágenes de un artículo nuevo y al final se entrega un artículo usado. 
+Condiciones para la compra de artículos
+Crear una plataforma de comercio electrónico donde los compradores encuentren lo que buscan, es uno de los objetivos primordiales de tu compra.com por lo cual te especificamos lo que debes tener en cuenta: 
+Cuando compras un artículo debes estar de acuerdo con lo siguiente: 
+•	Eres responsable de leer todo el anuncio del artículo antes de hacer una oferta de compra o confirmar una compra. 
+•	Al elegir un anuncio que sea de tu conveniencia debes contactarte con el vendedor de dicho anuncio, solo si realmente estas interesado en dicha compra. 
+•	Tu oferta.com no se hace responsable de cualquier daño que pueda tener el artículo que desees comprar, ya que solo ofrecemos la oportunidad de que puedas encontrar lo que buscas con mayor facilidad. 
+Condiciones para la venta de artículos
+Así mismo es de nuestro interés que los vendedores logren sus objetivos de venta de una forma eficiente, por lo cual cuando vendes un artículo te debes comprometer a cumplir lo siguiente: 
+•	Ofrecer un artículo en buen estado, esto quiere decir, que, si ofreces un artículo usado, tú debes garantizar que dicho artículo se puede utilizar en lo que se amerita.
+•	Eres responsable de dar una descripción bien detallada del contenido de lo que es el artículo ofrecido para la venta.
+•	Debes indicar obligatoriamente el precio del producto que deseas vender, el cual debe ser: en lempiras o dólares solamente.
+•	Es necesario que al publicar tu artículo para la venta pongas al menos una imagen del mismo, ya que de esta manera haces tú anuncio más amigable a las personas que visitan la página.
+Condiciones para la venta de servicios
+De igual forma, cuando tu objetivo radica en publicar un anuncio para dar un servicio, nos interesa que tomes en cuenta lo siguiente: 
+•	El servicio que se brindará debe ser legal. 
+•	Es importante colocar el costo del servicio que quieres brindar, ya que éste es muy importante para que los usuarios que vean el anuncio estén consientes al momento de contactarse contigo. 
+•	Siempre debes colocar al menos una imagen para ilustrar el tipo de servicio que ofreces.
+
             </div>
         </div>
     </div>
