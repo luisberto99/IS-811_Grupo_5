0d6318db7e875e90be77501a9deea8625ab6dfcc
Alberto Medina <luisitomedina95@gmail.com>
2021-04-14 20:09:35 -0600

    Add AdvertControllerHeidy

--- app/Http/Controllers/advertController.php
+++ app/Http/Controllers/advertController.php
@@ -9,6 +9,7 @@ use App\Models\Category;
 use App\Models\Currency;
 use App\Models\Departament;
 use App\Models\Product;
+use App\Models\Qualification;
 use App\Models\Township;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -30,26 +31,35 @@ class AdvertController extends Controller
         $township = $township->name;
         $department = $department->name;
         $user = User::find($advert->user_id);
+        $townshipUser = Township::find($user->township_id);
+        $departmentUser = Departament::find($townshipUser->departament_id);
+        $townshipUser = $townshipUser->name;
+        $departmentUser = $departmentUser->name;
         $AlladdsUser = Advert::where('user_id',$user->id )->get()->count();
         $adsActive = Advert::where('user_id',$user->id )->where('advert_status_id', 1)->get()->count();
         $coment=  AdvertComment::where('advert_id',$id)->whereRaw('parent_id = id')-> get();
-        $photos = AdvertPhoto::where('advert_id',$id)->get();   
+        $photos = AdvertPhoto::where('advert_id',$id)->get(); 
+
+        // valoracion
+        $val = DB::table('qualifications')->select(DB::raw('SUM(qualification) / ((COUNT(qualification) * 5) / 100) as rating')) ->where('qualified',$user->id)->get() ;
         
+        
+           
         Carbon::setLocale('es');
         
          $dat = new Carbon($user->created_at);
          
-         $dat1= [$dat->getTranslatedMonthName(), $dat->year];
+         $userDt= [$dat->getTranslatedMonthName(), $dat->year];
          $dat2 = new Carbon($advert->creation_date);
         
 
-         $dat3=[$dat2->day, $dat->getTranslatedMonthName(), $dat2->year];
+         $advertDt=[$dat2->day, $dat->getTranslatedMonthName(), $dat2->year];
         
 
        
         
 
-        return view('advert.show', compact('dat1','dat3', 'advert', 'category', 'coment','adProduct','currency','township','department','user','adsActive','AlladdsUser','photos'));
+        return view('advert.show', compact('val','townshipUser','departmentUser','userDt','advertDt', 'advert', 'category', 'coment','adProduct','currency','township','department','user','adsActive','AlladdsUser','photos'));
 
 
     }
