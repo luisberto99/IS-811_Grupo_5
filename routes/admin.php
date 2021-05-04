3e65beeb6b58ffb430a442677781fa0fbeea4bb5
Wilson <13283998+wPaniagua@users.noreply.github.com>
2021-05-03 23:51:39 -0600

    Add files via upload

---
+++ routes/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Admin\categoriasController;
+use App\Http\Controllers\Admin\estadisticasController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\UserController;
+
+Route::get('home', [HomeController::class, 'index'])->middleware('can:admin.home')->name('admin.home');
+
+Route::resource('users', UserController::class)->only('index','edit','update')->middleware('can:admin.user')->names('admin.users');
+Route::resource('estadisticas', estadisticasController::class)->middleware('can:admin.estadisticas')->names('admin.estadistica');
+Route::resource('categorias', categoriasController::class)->middleware('can:admin.categories')->names('admin.categorias');
+
+Route::get('denuncias', [HomeController::class, 'show'])->middleware('can:admin.moderador')->name('admin.denuncias');
+Route::get('mostrar/{denuncia}', [HomeController::class, 'mostrar'])->middleware('can:admin.moderador')->name('admin.mostrar');
---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Http\Controllers\advertController;
+use App\Http\Controllers\advertControllers;
+use App\Http\Controllers\advertUserController;
+use App\Http\Controllers\PerfilController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('perfiles/{perfil}', [PerfilController::class, 'show'])->name('perfiles.show');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/nuevo', function () {
+    return view('advert.nuevo');
+})->name('nuevo');
+
+
+Route::get('advertsUser/show/f{fill?}',[advertUserController::class, 'filterUser'])->name('advertsUser');
+Route::get('adverts/show/f{fill?}',[advertUserController::class, 'filter'])->name('adverts');
+
+/* Route::get('advert{id}?', function ($id = null) {
+    return "work $id";
+}); */
+
+/*Route::get('advert/{id}', function ($id) {
+    return Advert::find($id);
+})->name('advert.show');*/
+
+Route::get('users/fill{id}', function ($id) {
+    return User::find($id);
+})->name('user.show');
+
+Route::get('advertsUser/{anuncio}/edit', [advertUserController::class, 'edit'])->name('advertsUser.edit');
+Route::get('perfiles', [PerfilController::class, 'store'])->name('perfiles.store');
+Route::post('perfiles/calificacion', [PerfilController::class, 'storeCalificacion'])->name('perfiles.calificacion');
+
+Route::get('adverts/{anuncio}/edit', [advertControllers::class, 'edit'])->name('adverts.edit');
+
+Route::get('/advert/{show}',[ AdvertController::class, 'show'])->name('advert.show');
+Route::middleware(['auth:sanctum', 'verified'])->post('/comment',[AdvertController::class, 'storeComment'])->name('advert.comment');
+
+Route::get('mycategories/{id}', [CategoriaController::class, 'listarCategorias'])->name('categorias.show');
+Route::get('mycategories/{id_categoria}/{id_user}', [CategriaController::class, 'guardar'])->name('categoria.guardar');
+Route::get('mycategories/eliminar/{id}/{idUser}', [CategriaController::class, 'eliminar'])->name('categoria.eliminar');
