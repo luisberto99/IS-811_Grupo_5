7201aa4ab086ac39a1558819abda66071c6a4810
Wilson <13283998+wPaniagua@users.noreply.github.com>
2021-05-03 23:34:01 -0600

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'admin.categorias' => 'App\\Http\\Livewire\\Admin\\Categorias',
+  'admin.estadisticas' => 'App\\Http\\Livewire\\Admin\\Estadisticas',
+  'admin.users' => 'App\\Http\\Livewire\\Admin\\Users',
+  'admin.users-index' => 'App\\Http\\Livewire\\Admin\\UsersIndex',
+  'categorias.suscrito' => 'App\\Http\\Livewire\\Categorias\\Suscrito',
+  'denuncia' => 'App\\Http\\Livewire\\Denuncia',
+  'formulario-anuncio' => 'App\\Http\\Livewire\\FormularioAnuncio',
+  'landing-carrousels' => 'App\\Http\\Livewire\\LandingCarrousels',
+  'menu-categorias' => 'App\\Http\\Livewire\\MenuCategorias',
+  'menu-departamentos' => 'App\\Http\\Livewire\\MenuDepartamentos',
+  'navigation-menu-guest' => 'App\\Http\\Livewire\\NavigationMenuGuest',
+);
